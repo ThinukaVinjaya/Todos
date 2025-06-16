0b4295acfd53b563abf97efaa74d7ca13e8977ae
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2025-06-16 22:06:09 +0530

    First commit

---
+++ src/main/java/com/thinuka/todos/entity/Authority.java
@@ -0,0 +1,22 @@
+package com.thinuka.todos.entity;
+
+import jakarta.persistence.Embeddable;
+import org.springframework.security.core.GrantedAuthority;
+
+@Embeddable
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    private Authority(){}
+
+    public Authority(String authority){
+        this.authority = authority;
+    }
+
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+}
